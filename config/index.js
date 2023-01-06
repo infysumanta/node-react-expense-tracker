8b4dffc49f3602c148648ffe0cf92fcfdfd8a5ac
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-06 14:39:32 +0530

    backend server config

---
+++ config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  PORT: process.env.PORT || 5000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  MONGO_URI:
+    process.env.MONGO_URI || "mongodb://localhost/node-react-kanban-app",
+  JWT_SECRET:
+    process.env.JWT_SECRET ||
+    "0e96a7ddb3d364ca83a792a0034bef85759451b149bc271dc3034860a7d6c26f",
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+require("./config/db").connectDB;
+const config = require("./config");
+const app = require("./app");
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on Port ${config.PORT} 🔥`);
+});
